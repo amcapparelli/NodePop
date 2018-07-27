69b813f13e07a1b4f67c8da58b2b915c24176ca9
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-07-27 21:51:17 +0200

    creo la vista de la página principal con los filtros para mostrar resultados segun precios, limit, , pagina, etc.

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,30 @@ var router = express.Router();
 const Ads = require('../lib/createModels')
 
 /* GET home page. */
+
 router.get('/', async (req, res, next) => {
   try {
-    const allAds = await Ads.find()
-    const adsToShow = allAds.map(item =>  item.productName + ' ' + item.price )  
+    // Recojo los posibles filtros de la URL
+    const filter = {}
+    const tag = req.query.tag
+    const limit = parseInt(req.query.limit)
+    const page = parseInt(req.query.page)
+    const sort = req.query.sort
+    const priceMin = parseInt(req.query.pricemin)
+    const priceMax = parseInt(req.query.pricemax)
+    
+    //si se incluyen tags, busca en el array de tags para mostrar coincidencias
+    if (tag){
+      filter.tags = tag
+    }
+
+    /* Consulto a la BD con los filtros y recojo resultados en la variable adsToShow 
+    con el nombre y el precio del producto */
+    const allAds = await Ads.show(filter, limit, page, sort, priceMin, priceMax)
+    const adsToShow = allAds.map
+      (item => item.productName 
+              + ' ' 
+              + item.price +'€')
       res.render('index', {
         title: 'NodePop',
         adlist: adsToShow
@@ -14,7 +34,6 @@ router.get('/', async (req, res, next) => {
   } catch (err) {
       next(err)
   }
-  
 });
 
 module.exports = router;
