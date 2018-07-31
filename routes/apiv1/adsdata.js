842a76564eed1a3dad761dd6248bce2825755484
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-07-31 19:52:13 +0200

    Agrego validaciones para las peticiones por metodo POST

--- routes/apiv1/adsdata.js
+++ routes/apiv1/adsdata.js
@@ -2,12 +2,20 @@
 
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator/check')
 const Ads = require('../../lib/createModels')
 const getParams = require('../../lib/functions')
 
+
 router.get('/', getParams)
 
-router.post('/', async (req, res, next) => {
+router.post('/', [
+    check('forSale', 'Must be bollean').isBoolean(),
+    check('price').isNumeric(),
+    check('image').isURL(), 
+    check('productName').isAlphanumeric(), 
+    check('tags').isArray()
+    ] ,async (req, res, next) => {
     try{
         const newAdData = req.body
         const newAd = new Ads(newAdData)
