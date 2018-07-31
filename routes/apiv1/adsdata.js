54c701925cac42a9727f3cef0c5a67460dd8887e
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-07-31 13:11:52 +0200

    Creo el archivo functions para reutilizar la función que recoge las peticiones get a la api y a la pagina de inicio

--- routes/apiv1/adsdata.js
+++ routes/apiv1/adsdata.js
@@ -3,51 +3,19 @@
 const express = require('express')
 const router = express.Router()
 const Ads = require('../../lib/createModels')
+const getParams = require('../../lib/functions')
 
-router.get('/', async (req, res, next) => {
-    try {
-        // Posibles filtros de la URL
-        const filter = {}
-        const tag = req.query.tag
-        const limit = parseInt(req.query.limit)
-        const page = parseInt(req.query.page)
-        const sort = req.query.sort
-        const priceMin = parseInt(req.query.pricemin)
-        const priceMax = parseInt(req.query.pricemax)
-        const forsale = req.query.forsale
-        const prodname = req.query.prodname
-         
-        if (tag){
-          filter.tags = tag
-        }
-        if (prodname) {
-            filter.productName = {$regex: "^" + prodname}
-        }
-    
-        /* Extraigo de la base de datos los anuncios según los filtros y todos los tags disponibles */
-        const allAds = await Ads.show(filter, limit, page, sort, priceMin, priceMax, forsale)
-        const allTags = await Ads.distinct("tags")
+router.get('/', getParams)
 
-        /* Respondo con un JSON */
-          res.json ({
-              success: true, 
-              result: allAds, allTags
-            })
-
-      } catch (err) {
-          next(err)
-      }
-    })
-
-    router.post('/', async (req, res, next) => {
-        try{
-            const newAdData = req.body
-            const newAd = new Ads(newAdData)
-            const newAdSaved = await newAd.save()
-            res.json({success: true, result: newAdSaved})
-        } catch (err) {
-            next(err)
-        }
-    })
+router.post('/', async (req, res, next) => {
+    try{
+        const newAdData = req.body
+        const newAd = new Ads(newAdData)
+        const newAdSaved = await newAd.save()
+        res.json({success: true, result: newAdSaved})
+    } catch (err) {
+        next(err)
+    }
+})
 
   module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const Ads = require('../lib/createModels')
+const getParams = require('../lib/functions')
 
 /* GET home page. */
 
-router.get('/', async (req, res, next) => {
-  try {
-    // Recojo los posibles filtros de la URL
-    const filter = {}
-    const tag = req.query.tag
-    const limit = parseInt(req.query.limit)
-    const page = parseInt(req.query.page)
-    const sort = req.query.sort
-    const priceMin = parseInt(req.query.pricemin)
-    const priceMax = parseInt(req.query.pricemax)
-    const forsale = req.query.forsale
-    const prodname = req.query.prodname
-    
-    if (tag){
-      filter.tags = tag
-    }
-    if (prodname) {
-      filter.productName = {$regex: "^" + prodname}
-    }
-    /* Extraigo de la base de datos los anuncios según los filtros y todos los tags disponibles */
-    const allAds = await Ads.show(filter, limit, page, sort, priceMin, priceMax, forsale)
-    const allTags = await Ads.distinct("tags")
-  
-    res.render('index', {
-      title: 'NodePop',
-      adlist: allAds, 
-      tagsList: allTags.sort()
-    })
-    
-  } catch (err) {
-      next(err)
-  }
-});
+router.get('/', getParams)
 
 module.exports = router;
