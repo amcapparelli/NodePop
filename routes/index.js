e676e11fb5ed81df8d0ace9c5bfff65db171c035
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-08-04 19:34:43 +0200

    creo una validación para el parámetro pricerange con una RegEx

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
+'use strict'
+
 var express = require('express');
 var router = express.Router();
+const validations = require('../lib/validations')
 const getParams = require('../lib/functions')
 
 /* GET home page. */
 
-router.get('/', getParams)
+router.get('/', validations, getParams)
 
 module.exports = router;
