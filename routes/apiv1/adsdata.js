a27224b493c4c474209131c45dd110d4e0359c87
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2018-10-30 20:07:16 +0100

    Create Thumbnails using sharp

--- routes/apiv1/adsdata.js
+++ routes/apiv1/adsdata.js
@@ -9,6 +9,7 @@ const validationsGET = require('../../lib/validationsGET')
 const validationsPOST = require('../../lib/validationsPOST')
 const jwtAuth = require('../../lib/jwtAuth')
 const upload = require('../../lib/uploadImagesConfig')
+const sharp = require('sharp');
 
 
 router.use(jwtAuth())
@@ -21,6 +22,17 @@ router.post('/',  upload.single('imageFile'), validationsPOST, async (req, res, 
         const newAdData = req.body
         const newAd = new Ads(newAdData)
         const newAdSaved = await newAd.save()
+
+        sharp(req.file.path)
+        .resize (100, 100)
+        .toFile('imagesUploaded/thumbnails/thumbnail_'+req.file.originalname, function(err) {
+            if (err){
+                console.log('Error al crear el thumbnail', err)
+                return
+            } 
+            console.log('Thumbnail creado')
+        })
+
         res.json({success: true, result: newAdSaved})
     } catch (err) {
         next(err)
